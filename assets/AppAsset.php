80c3ac770bbe9eddf584d703bb25232d9fbe98a7
neshika <stepanovaml@mail.ru>
2023-01-08 02:16:48 +0300

    контейнер

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+    'css/bootstrap.min.css',
+    'css/font-awesome.min.css',
+    'css/prettyPhoto.css',
+    'css/price-range.css',
+    'css/animate.css',
+	'css/main.css',
+	'css/responsive.css',
     ];
     public $js = [
+        'js/jquery.js',
+        'js/bootstrap.min.js',
+        'js/jquery.scrollUp.min.js',
+        'js/price-range.js',
+        'js/jquery.prettyPhoto.js',
+        'js/main.js',
+        //из комментов к уроку
+        ['js/html5shiv.js', 'condition' => 'lt IE 9', 'position' => \yii\web\View::POS_HEAD],
+        ['js/respond.min.js', 'condition' => 'lt IE 9', 'position' => \yii\web\View::POS_HEAD],
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+        'yii\bootstrap5\BootstrapPluginAsset'
     ];
 }
